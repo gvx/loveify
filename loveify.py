5a36a712e5be949a23558efe9bba246c3110a906
gvx <gyvox.public@gmail.com>
2009-05-21 01:33:18 +0200

    Made open_file a reference instead of a function
    This means less function call overhead, and no extra function creation

--- loveify.py
+++ loveify.py
@@ -8,13 +8,11 @@ out = zipfile.ZipFile(outfile, 'w')
 if zipfile.is_zipfile(infile):
 	arch = zipfile.ZipFile(infile)
 	names = arch.namelist()
-	def open_file(name):
-		return arch.open(name)
+	open_file = arch.open
 else:
 	arch = tarfile.open(infile)
 	names = arch.getnames()
-	def open_file(name):
-		return arch.extractfile(name)
+	open_file = arch.extractfile
 
 for name in names:
 	f = open_file(name)
