5e0cc0e34c8137597057054ce05aae9b30b5f8cf
robin User <robin@ubuntu.ubuntu-domain>
2009-05-21 00:52:50 +0200

    first version

---
+++ loveify.py
@@ -0,0 +1,26 @@
+import zipfile
+import tarfile
+import sys
+
+infile = sys.argv[1]
+outfile = sys.argv[2]
+out = zipfile.ZipFile(outfile, 'w')
+if zipfile.is_zipfile(infile):
+	arch = zipfile.ZipFile(infile)
+	names = arch.namelist()
+	def open_file(name):
+		return arch.open(name)
+else:
+	arch = tarfile.open(infile)
+	names = arch.getnames()
+	def open_file(name):
+		return arch.extractfile(name)
+
+for name in names:
+	f = open_file(name)
+	n = name.split('/', 1)[1]
+	if n:
+		out.writestr(n, f.read())
+	f.close()
+arch.close()
+out.close()
